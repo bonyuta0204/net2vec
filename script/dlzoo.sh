3613a37c7c1563b426786e3f3c458cd77d06c943
Ruth Fong <ruthcfong@gmail.com>
2018-01-09 15:32:45 +0000

    download select moddels

--- script/dlzoo.sh
+++ script/dlzoo.sh
@@ -6,16 +6,18 @@ cd "$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 declare -a MODELS=(
   "caffe_reference_imagenet"      # alexnet-imagenet
-  "caffe_reference_places205"     # alexnet-places205
+  #"caffe_reference_places205"     # alexnet-places205
   "caffe_reference_places365"     # alexnet-places365
   "vgg16_imagenet"                # vgg-imagenet
-  "vgg16_places205"               # vgg-places205
+  #"vgg16_places205"               # vgg-places205
   "vgg16_places365"               # vgg-places365
   "googlenet_imagenet"            # googlenet-imagenet
-  "googlenet_places205"           # googlenet-places205
+  #"googlenet_places205"           # googlenet-places205
   "googlenet_places365"           # googlenet-places365
   "resnet-152-torch-imagenet"     # resnet-imagenet
   "resnet-152-torch-places365"    # resnet-places365
+  "densenet161_imagenet"
+  "densenet161_places365"
 )
 
 for MODEL in "${MODELS[@]}"
